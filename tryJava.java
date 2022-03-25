0e8fc6c7a77b374e3d21ba7c2e5311e37b623a1b
Shreya Kakade <69631253+Shreya282@users.noreply.github.com>
2022-03-25 09:51:01 +0530

    Create tryJava.java

---
+++ tryJava.java
@@ -0,0 +1,5 @@
+class Add{
+  public static void main(String args[]
+                          {
+                           System.out.println("Hello this is the file for git jenkin integration");
+                          }
